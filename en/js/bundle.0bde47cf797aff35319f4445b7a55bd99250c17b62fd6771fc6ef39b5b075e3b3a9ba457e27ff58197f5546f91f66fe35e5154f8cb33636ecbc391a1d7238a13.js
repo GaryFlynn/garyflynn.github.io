9588e10d0175dde1a54d051fcd73fb5e961305ac
GaryFlynn <GaryFlynn@users.noreply.github.com>
2023-03-09 06:07:57 +0000

    deploy: GaryFlynn/garyflynn.com-source@d4bccde5de10c82fbab0f9eaf86cb8415bf83e23

--- en/js/bundle.0bde47cf797aff35319f4445b7a55bd99250c17b62fd6771fc6ef39b5b075e3b3a9ba457e27ff58197f5546f91f66fe35e5154f8cb33636ecbc391a1d7238a13.js
+++ en/js/bundle.0bde47cf797aff35319f4445b7a55bd99250c17b62fd6771fc6ef39b5b075e3b3a9ba457e27ff58197f5546f91f66fe35e5154f8cb33636ecbc391a1d7238a13.js
@@ -8,7 +8,7 @@ const scaleImageClass = 'image-scale';
 const pageHasLoaded = 'DOMContentLoaded';
 const imageAltClass = 'img_alt'
 
-const baseURL = 'https://garyflynn.com';
+const baseURL = 'https://example.com/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
